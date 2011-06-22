da8de1d8b0259d4fa4c958b1ff6a33c25728c503
Erwin Coumans <erwin.coumans@gmail.com>
2011-06-22 14:47:42 -0700

    add wxWidgets-2.9.1 with premake build system support and 2 tests

---
+++ build/premake4.lua
@@ -0,0 +1,42 @@
+solution "0MySolution"
+
+	configurations {"Release", "Debug"}
+	configuration "Release"
+		flags { "Optimize", "StaticRuntime", "NoRTTI", "NoExceptions"}
+	configuration "Debug"
+		flags { "Symbols", "StaticRuntime" , "NoRTTI", "NoExceptions"}
+	platforms {"x32", "x64"}
+
+	configuration "x64"		
+		targetsuffix "_64"
+	configuration {"x64", "debug"}
+		targetsuffix "_x64_debug"
+	configuration {"x64", "release"}
+		targetsuffix "_x64"
+	configuration {"x32", "debug"}
+		targetsuffix "_debug"
+
+
+	dofile ("findOpenCL.lua")
+	dofile ("findDirectX11.lua")
+	
+	language "C++"
+	location "build"
+	targetdir "bin"
+
+--	include "../opencl/opengl_interop"
+--	include "../opencl/integration"
+--	include "../opencl/primitives/AdlTest"
+--	include "../rendering/GLSL_Instancing"
+--	include "../opencl/basic_initialize"
+
+	include "../wxwidgets/wxWidgetsGLTest"
+	include "../wxwidgets/treectrl"
+-- for Visual Studio 2008 you don't need to build wxAll, it is already precompiled
+
+	include "../wxwidgets/wxWidgets-2.9.1/wxCMake/wxAll"
+	
+	
+	
+	
+	
